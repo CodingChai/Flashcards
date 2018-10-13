2112a54f056fba9b5a78189b53b2a665ce3c0d8a
George Lu <georgelu@georges-macbook-pro.local>
2018-10-13 16:16:49 -0500

    Simple Flashcards lab done

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var AnswerLabel: UILabel!
+    @IBOutlet weak var QuestionLabel: UILabel!
+    
+    @IBOutlet var tapOnFlashCard: UITapGestureRecognizer!
+    
+    var firstTime: Bool!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        firstTime=true;
     }
 
-
+    @IBAction func tappedFlashCard(_ sender: Any) {
+        if(firstTime){
+            QuestionLabel.isHidden=true;
+            firstTime=false;
+        }
+        else if(QuestionLabel.isHidden==true && AnswerLabel.isHidden==false){
+            AnswerLabel.isHidden=true;
+            QuestionLabel.isHidden=false;
+        }
+        else{
+            AnswerLabel.isHidden=false;
+            QuestionLabel.isHidden=true;
+        }
+    }
+    
 }
 
